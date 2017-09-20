d7dd1965dd0d4d5839dff9e5ce0e163b4197a14e
leonard kakande <kakandeleonard@gmail.com>
2017-09-20 10:31:15 +0300

    adding the read method

--- update_demo_plots.rb
+++ update_demo_plots.rb
@@ -7,3 +7,10 @@ def write_to_file
   out_file.write(indata)
   out_file.close
 end
+
+def read_from_file
+  in_file = open("#{Dir.home}/demo_plots.txt")
+  indata = in_file.read.to_s
+  in_file.close
+  indata.split('<>')
+end
